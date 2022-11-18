1db44c51fb0780227d44aa7f3ca11e72de6af27b
cbspace <24204459+cbspace@users.noreply.github.com>
2022-11-19 07:06:46 +1000

    Progress on day1 part 1

--- 2018-Assembly/README.md
+++ 2018-Assembly/README.md
@@ -1,8 +1,8 @@
-Ok here we go, attempting AOC in 64 bit assembly using NASM. Although NASM is cross platform the code will be calling Linux interrupts directly and therefore will be limited to Linux ELF format.
+Ok here we go, attempting AOC in 64 bit assembly using NASM. Although NASM is cross platform the code will be calling Linux syscalls directly and therefore will be limited to Linux ELF format.
 
 Requires:
 
-The NASM compiler: `sudo apt install nasm`
+The NASM assembler: `sudo apt install nasm`
 
 LD linker: `sudo apt install build-essential`
 
--- 2018-Assembly/day1.asm
+++ 2018-Assembly/day1.asm
@@ -1,10 +1,44 @@
-section .data
-    ;Let's do the simple example of "+1, -2, +3, +1"
-    numbers: dw 1, -2, 3, 1 
-    length: equ $-numbers
+    global _start
 
     section .text
-        global _start
-
     _start:
-        
+        mov     rcx, 30h                  ; ASCII '0'
+        mov     rdx, numbers              ; pointer to number to write
+        mov     r8, length
+        xor     r9, r9                    ; counter
+    
+    .add_loop:
+        add     rcx, [rdx]
+        add     rdx, 4                    ; move pointer
+        add     r9, 4                     ; increment counter
+        cmp     r8, r9
+        jne     .add_loop
+
+        mov     [total], rcx
+        mov     rsi, total
+        call    print
+
+        mov     rsi, newline
+        call    print
+
+    .exit:
+        mov       rax, 60                 ; system call for exit
+        xor       rdi, rdi                ; exit code 0
+        syscall                           ; invoke operating system to exit
+
+    ; Print contents of byte at rsi
+    print:
+        mov       rax, 1                  ; system call for write
+        mov       rdi, 1                  ; file handle 1 is stdout
+        mov       rdx, 1                  ; number of bytes
+        syscall
+        ret
+
+    section .data
+        ;Let's do the simple example of "+1, -2, +3, +1"
+        numbers:    dd 1, -2, 3, 1
+        length:     equ $-numbers
+        newline:    db 10
+    
+    section .bss
+        total resb 1
