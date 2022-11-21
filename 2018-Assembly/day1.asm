8b2e62d2d323ba7c4b5a0d831368f0b93d08730f
cbspace <24204459+cbspace@users.noreply.github.com>
2022-11-22 06:03:28 +1000

    Tidy up and change register usage

--- 2018-Assembly/day1.asm
+++ 2018-Assembly/day1.asm
@@ -5,32 +5,32 @@
     %include "string.asm"
 
     _start:
-        mov     rcx, 0
-        mov     rdx, numbers              ; pointer to number to write
-        mov     r8, length
-        xor     r9, r9                    ; counter
+        mov     rbx, 0            ; total
+        mov     rbp, numbers      ; pointer to number to write
+        mov     r12, length       ; length of input array
+        xor     r13, r13          ; counter
     
     .add_loop:
-        add     rcx, [rdx]
-        add     rdx, 8                    ; move pointer
-        add     r9, 8                     ; increment counter
-        cmp     r8, r9
+        add     rbx, [rbp]
+        add     rbp, 8            ; move pointer
+        add     r13, 8            ; increment counter
+        cmp     r12, r13
         jne     .add_loop
 
-        mov     [total], rcx
-        mov     rdi, [total]
+        mov     [total], rbx      ; save total
+        mov     rsi, [total]      ; load total
 
-        call    itoa
-        mov     rsi, itoa_result_str
+        call    int_to_string
+        mov     rsi, result_str
         call    print
 
         mov     rsi, newline
         call    print
 
     .exit:
-        mov       rax, 60                 ; system call for exit
-        xor       rdi, rdi                ; exit code 0
-        syscall                           ; invoke operating system to exit
+        mov       rax, 60         ; system call for exit
+        xor       rdi, rdi        ; exit code 0
+        syscall                   ; invoke operating system to exit
 
     section .data
         ;Let's do the simple example of 4 numbers
@@ -39,8 +39,8 @@
         newline:    db 10
     
     section .bss
-        total           resq 1
-        itoa_result_str resb 20
+        total       resq 1
+        result_str  resb 20
 
 ; Register usage
 ; rax - Caller-saved register, Function return values
--- 2018-Assembly/inc/string.asm
+++ 2018-Assembly/inc/string.asm
@@ -7,50 +7,50 @@
     ; Print contents of bytes from [rsi]
     ; Note: The string length is fixed
     print:
-        mov       rax, 1                  ; system call for write
-        mov       rdi, 1                  ; file handle 1 is stdout
-        mov       rdx, 3                  ; number of bytes
+        mov     rax, 1            ; system call for write
+        mov     rdi, 1            ; file handle 1 is stdout
+        mov     rdx, 3            ; number of bytes
         syscall
         ret
 
-    ; Integer to ASCII: Convert int64 at RDI to ASCII
-    ; and save in itoa_result_str - currently signed only
+    ; Integer to string: Convert int64 at rsi to string
+    ; and save in result_str - currently signed only
     ; Note the string length is fixed at 3 digits
-    itoa:
-        mov rbx, 2                        ; 2 means 10^2 divisor or 0 - 999 range
-        mov r10, itoa_result_str          ; pointer to string
+    int_to_string:
+        mov     rbx, 2            ; 2 means 10^2 divisor or 0 - 999 range
+        mov     r10, result_str   ; pointer to string
     .loop:
-        mov r11, rbx                      ; loop limit to r11
-        mov r9, r11                       ; divisor exponent to r9
-        call pow10                        ; get the divisor
-        mov rcx, rax                      ; divisor to rcx
-        mov rdx, 0                        ; reset rdx (high reg)
-        mov rax, rdi                      ; load low reg
-        div rcx                           ; rax: quotient, rdx: remainder
-        add rax, 30h                      ; convert to ascii char
-        mov [r10], rax                    ; store in string
-        mov rdi, rdx                      ; remainder becomes new number
-        inc r10                           ; increment string pointer
-        test r11, r11                     ; test r11 and set zf accordingly
-        jz .done
-        dec r11                           ; decrement loop counter
-        dec rbx                           ; decrement divisor
-        jmp .loop
+        mov     r11, rbx          ; loop limit to r11
+        mov     r9, r11           ; divisor exponent to r9
+        call    pow10             ; get the divisor
+        mov     rcx, rax          ; divisor to rcx
+        mov     rdx, 0            ; reset rdx (high reg)
+        mov     rax, rsi          ; load low reg
+        div     rcx               ; rax: quotient, rdx: remainder
+        add     rax, 30h          ; convert to ascii char
+        mov     [r10], rax        ; store in string
+        mov     rsi, rdx          ; remainder becomes new number
+        inc     r10               ; increment string pointer
+        test    r11, r11          ; test r11 and set zf accordingly
+        jz      .done
+        dec     r11               ; decrement loop counter
+        dec     rbx               ; decrement divisor
+        jmp     .loop
     .done:
-        mov [r10], byte 0                 ; null terminate string
+        mov     [r10], byte 0     ; null terminate string
         ret
 
     ; Take int64 in r9 and return 10^r9 in rax
     ; Fixme - return 0 on overflow
     pow10:
-        mov r8, 10
-        mov rax, 1
+        mov     r8, 10
+        mov     rax, 1
     .multiply_loop:
-        cmp r9, 0
-        jz .done
-        mul r8                    ; rax = rax * r8
-        dec r9
-        jmp .multiply_loop
+        cmp     r9, 0
+        jz      .done
+        mul     r8                ; rax = rax * r8
+        dec     r9
+        jmp     .multiply_loop
     .done:
         ret
 
