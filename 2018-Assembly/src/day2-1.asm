9aca34411c7d196835a1296cad0ed32a7e71daa1
cbspace <24204459+cbspace@users.noreply.github.com>
2022-11-26 21:05:26 +1000

    Progress on day 2 part 1

--- 2018-Assembly/src/day2-1.asm
+++ 2018-Assembly/src/day2-1.asm
@@ -4,29 +4,50 @@
 
     section .text
 
-    %include "string.asm"
+    %include     "string.asm"
+    %define      BOX_ID_LENGTH 6
 
     _start:
-        
+        xor     r12, r12                ; number of boxes containing a letter repeated twice
+        xor     r13, r13                ; number of boxes containing a letter repeated thre times
+        mov     r12, box_ids            ; pointer to first box_id
+        xor     rbx, rbx                ; loop counter
+
+    .loop:
+        call    check_box
+
+        mov     rdx, 6                  ; load str len
+        mov     rsi, r12                ; load string
+        call    print                   ; temp print
+        call    printcr
+
+        add     r12, BOX_ID_LENGTH      ; incrememnt the pointer
+        add     rbx, BOX_ID_LENGTH      ; increment counter
+        cmp     rbx, length             ; check if we reached the end
+        jne     .loop
 
     .exit:
-        mov       rax, 60               ; system call for exit
-        xor       rdi, rdi              ; exit code 0
+        mov     rax, 60                 ; system call for exit
+        xor     rdi, rdi                ; exit code 0
         syscall                         ; invoke operating system to exit
+
+    ; Check for repeated characters in box_id pointed to by rdi
+    check_box:
+
+        ret
 
     section .data
         ; This is a bit of a hack, adding the (reformatted) input file
         ; as an include. The next step is to open the file using assembley!
-        boxids:
-        %include "day2_test.txt"
-        length:     equ ($-boxids)/6
-        newline:    db 10
+        box_ids:
+        %include        "day2_test.txt"
+        length:         equ ($-box_ids)    ; size of box_ids
+        newline:        db 10
     
     section .bss
         ; For simplicity this section is aligned @ 8 bytes!
         result_str_len  resq 1
         result_str      resb 24
-        freq_array      resq 1000 ; the array is quite large...
 
 ; Register usage
 ; rax - Caller-saved register, Function return values
