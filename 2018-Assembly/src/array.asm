335b4a2aa87c831384d4b4a6189f70a44faef27c
cbspace <24204459+cbspace@users.noreply.github.com>
2022-12-01 16:23:19 +1000

    Add array functions as separate include

---
+++ 2018-Assembly/src/array.asm
@@ -0,0 +1,53 @@
+; Include file for array functions
+; By Craig Brennan 2022
+
+%ifndef ARRAY_ASM
+    %define ARRAY_ASM
+
+    ; search array for value and return offset of first occurance in rax
+    ; input array is pointed to by rdi with length in [rdi - 8]
+    ; value to compare stored in rbx
+    ; return -1 when not found
+    array_index:
+        mov       r9, [rdi - 8]         ; array length
+        test      r9, r9                ; is the array empty?
+        jz        .not_found            ; yes so leave
+        xor       r10, r10              ; counter = 0
+    .loop:
+        mov       r8, [rdi]             ; load array value
+        cmp       r8, rbx               ; compare to input value
+        je        .found
+        add       r10, 8                ; increase counter
+        add       rdi, 8                ; increase pointer
+        cmp       r10, r9               ; are we at the end of array?
+        jne       .loop                 ; no, keep going
+    .not_found:
+        mov       rax, -1               ; not found, return -1
+        ret
+    .found:
+        mov       rax, r10
+        ret
+
+    ; search array for value and return number of occurances in rax
+    ; input array is pointed to by rdi with length in [rdi - 8]
+    ; value to compare stored in rbx
+    array_count:
+        mov       r9, [rdi - 8]         ; array length
+        test      r9, r9                ; is the array empty?
+        jz        .done                 ; yes so leave
+        xor       r10, r10              ; counter = 0
+        xor       rax, rax              ; number of occurances
+    .loop:
+        mov       r8, [rdi]             ; load array value
+        cmp       r8, rbx               ; compare to input value
+        jne       .not_match            ; value does not match
+        inc       rax                   ; match, increase count
+    .not_match:
+        add       r10, 8                ; increase counter
+        add       rdi, 8                ; increase pointer
+        cmp       r10, r9               ; are we at the end of array?
+        jne       .loop                 ; no, keep going
+    .done:
+        ret
+
+%endif
--- 2018-Assembly/src/day1-2.asm
+++ 2018-Assembly/src/day1-2.asm
@@ -1,6 +1,4 @@
-    ; Day1 Part 2: It works! This one chugs though, lucky it's written in assembly :)
-    ; There's probably a better algorithm to solve this that is more efficient.
-    ; Fun fact - This takes 1.95s to run on my machine and the python version takes 25.1s
+    ; Day1 Part 2: It works!
     
     global _start
 
@@ -8,77 +6,58 @@
 
     %include "file.asm"
     %include "string.asm"
+    %include "array.asm"
 
     _start:
-        call    file_open               ; open input file
-        mov     r15, input_numbers      ; pointer to number
-        mov     rbx, 0                  ; counter for number of inputs
+        call    file_open                   ; open input file
+        mov     r15, input_numbers          ; pointer to number
+        mov     rbx, 0                      ; counter for number of inputs
     .read_inputs:
-        call    file_read_line          ; read a line into buffer
-        test    rax, rax                ; test if end of file
-        jz      .done_reading           ; end of file reached
-        mov     rsi, read_buffer        ; load the string
-        call    string_to_int           ; convert to int
-        mov     [r15], rax              ; store the number
-        add     rbx, 8                  ; increment count
-        add     r15, 8                  ; increment pointer
+        call    file_read_line              ; read a line into buffer
+        test    rax, rax                    ; test if end of file
+        jz      .done_reading               ; end of file reached
+        mov     rsi, read_buffer            ; load the string
+        call    string_to_int               ; convert to int
+        mov     [r15], rax                  ; store the number
+        add     rbx, 8                      ; increment count
+        add     r15, 8                      ; increment pointer
         jmp     .read_inputs
     .done_reading:
-        call    file_close              ; close input file
-        mov     [input_numbers_len], rbx; store length
-        mov     rbx, 0                  ; total
-        mov     r14, freq_array         ; pointer to freq_array
-        mov     rbp, 0                  ; freq_array_length
+        call    file_close                  ; close input file
+        mov     [input_numbers_len], rbx    ; store length
+        mov     rbx, 0                      ; total
+        mov     r14, freq_array             ; pointer to freq_array
+        mov     qword [freq_array_length], 0; freq_array_length
     .loop_init:
-        mov     r15, input_numbers      ; pointer to number
-        xor     r13, r13                ; counter
+        mov     r15, input_numbers          ; pointer to number
+        xor     r13, r13                    ; counter
     .add_loop:
-        add     rbx, [r15]              ; increase total
-        add     r15, 8                  ; move pointer
-        add     r13, 8                  ; increment counter
-        call    search_array            ; check if number is in array
-        cmp     rax, 1                  ; compare return value
-        je      .found                  ; found a match
+        add     rbx, [r15]                  ; increase total
+        add     r15, 8                      ; move pointer
+        add     r13, 8                      ; increment counter
+        mov     rdi, freq_array             ; load frequency array pointer
+        call    array_index                 ; check if number is in array
+        cmp     rax, 0                      ; compare return value
+        jns     .found                     ; found a match
 
-        mov     [r14], rbx              ; store total in array
-        add     r14, 8                  ; move pointer
-        add     rbp, 8                  ; increment array length
+        mov     [r14], rbx                  ; store total in array
+        add     r14, 8                      ; move pointer
+        add     qword [freq_array_length], 8; increment array length
 
-        cmp     r13, [input_numbers_len]  ; test if end of list
-        jne     .add_loop               ; not yet, keep looping
-        jmp     .loop_init              ; at end, start list again
+        cmp     r13, [input_numbers_len]    ; test if end of list
+        jne     .add_loop                   ; not yet, keep looping
+        jmp     .loop_init                  ; at end, start list again
 
     .found:
-        mov     rsi, rbx                ; load total
-        call    int_to_string           ; convert to string
-        mov     rsi, result_str         ; load string
-        call    println                 ; print
+        mov     rsi, rbx                    ; load total
+        call    int_to_string               ; convert to string
+        mov     rsi, result_str             ; load string
+        call    println                     ; print
 
     .exit:
-        mov       rax, 60               ; system call for exit
-        xor       rdi, rdi              ; exit code 0
-        syscall                         ; invoke operating system to exit
-
-    ; search array and return 1 in rax if found
-    search_array:
-        test      rbp, rbp              ; is the array empty?
-        jz        .not_found            ; yes so leave
-        xor       r10, r10              ; counter = 0
-        mov       r11, freq_array       ; pointer to array
-    .loop:
-        mov       r8, [r11]             ; load array value
-        cmp       r8, rbx               ; compare to input value
-        je        .found
-        add       r10, 8                ; increase counter
-        add       r11, 8                ; increase pointer
-        cmp       r10, rbp              ; are we at the end of array?
-        jne       .loop                 ; no, keep going
-    .not_found:
-        mov       rax, 0                ; not found, return 0
-        ret
-    .found:
-        mov       rax, 1                ; return 1
-        ret
+        mov       rax, 60                   ; system call for exit
+        xor       rdi, rdi                  ; exit code 0
+        syscall                             ; invoke operating system to exit
 
     section .data
         file_path:  db  "./input/day1_input.txt", 0
@@ -93,6 +72,7 @@
         result_str          resb 24
         input_numbers_len   resq 1
         input_numbers       resq 1000      
+        freq_array_length   resq 1
         freq_array          resq 1000000   ; need a large array
 
 
