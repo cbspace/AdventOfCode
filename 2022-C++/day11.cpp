be67d01b55070221a3bd1d934423a1faca0c98ed
cbspace <24204459+cbspace@users.noreply.github.com>
2022-12-12 10:01:20 +1000

    Day 11 Part 1 complete

--- 2022-C++/day11.cpp
+++ 2022-C++/day11.cpp
@@ -2,21 +2,18 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
-enum class MonkeyOperator {
-    plus,
-    multiply
-};
-
 struct Monkey {
     vector<int> items;
-    MonkeyOperator opearator;
+    char opearator;
     int operand; // -1 = "old"
     int test_amount;
     int throw_to_if_true;
     int throw_to_if_false;
+    int inspect_total {0};
 };
 
 int main(int, char**) {
@@ -25,7 +22,7 @@ int main(int, char**) {
     vector<Monkey> monkeys;
     Monkey new_monkey;
 
-    ifs.open("input/day11_input1.txt", ios::in);
+    ifs.open("input/day11_input.txt", ios::in);
 
     while (!ifs.eof()) {
         getline(ifs, line_str);
@@ -44,7 +41,7 @@ int main(int, char**) {
             }
         }
         if (line_str.substr(2,10) == "Operation:") {
-            new_monkey.opearator = (line_str.substr(23,1) == "+") ? MonkeyOperator::plus : MonkeyOperator::multiply;
+            new_monkey.opearator = line_str.at(23);
             if (line_str.substr(25) == "old")
                 new_monkey.operand = -1;
             else
@@ -62,17 +59,30 @@ int main(int, char**) {
     }
     ifs.close();
 
-    for (int round = 1; round <= 1; round++) {
-        for (Monkey m : monkeys) {
+    for (int round = 1; round <= 20; round++) {
+        for (Monkey &m : monkeys) {
             for (int item : m.items) {
-                
-            }
-        }
+                int operand = (m.operand == -1) ? item : m.operand;
+                int new_value = ((m.opearator == '+') ? item + operand : item * operand) / 3;
+                if (new_value % m.test_amount == 0) {
+                    monkeys[m.throw_to_if_true].items.push_back(new_value);
 
+                } else {
+                    monkeys[m.throw_to_if_false].items.push_back(new_value);
+                }
+                m.inspect_total++;
+            }
+            m.items.clear();
+        }
     }
 
-    cout << "Part 1: " << endl;
-    cout << "Part 2: " << endl;
+    vector<int> item_totals;
+    for (Monkey m : monkeys) {
+        item_totals.push_back(m.inspect_total);
+    }
+
+    sort(item_totals.begin(), item_totals.end(), greater<int>());
+    cout << "Part 1: " << item_totals[0] * item_totals[1] << endl;
 
     return 0;
 }
