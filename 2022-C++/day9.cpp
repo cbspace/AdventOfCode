64781671a7506fedece7279533562c79f046eddb
cbspace <24204459+cbspace@users.noreply.github.com>
2022-12-13 22:48:45 +1000

    Day 9 Completed

--- 2022-C++/day9.cpp
+++ 2022-C++/day9.cpp
@@ -19,18 +19,20 @@ bool operator==(GridPoint const& lhs, GridPoint const& rhs){
 }
 
 void move_knot(vector<GridPoint> &, int, int, vector<GridPoint> &);
-void print_grid(vector<GridPoint> const&);
 
 int main(int, char**) {
     fstream ifs;
     string line_str;
-    vector<GridPoint> knots;
+    vector<GridPoint> knots1, knots2;
 
-    for (int i = 0; i <=9; i++)
-        knots.push_back(GridPoint());
+    for (int i = 0; i <= 1; i++)
+        knots1.push_back(GridPoint());
+    for (int i = 0; i <= 9; i++)
+        knots2.push_back(GridPoint());
 
-    vector<GridPoint> tail_visited;
-    tail_visited.push_back(knots[knots.size()-1]);
+    vector<GridPoint> tail_visited1, tail_visited2;
+    tail_visited1.push_back(knots1[0]);
+    tail_visited2.push_back(knots2[9]);
 
     ifs.open("input/day9_input.txt", ios::in);
 
@@ -66,26 +68,34 @@ int main(int, char**) {
         }
 
         for (int i = 0; i < move_amount; i++) {
-            knots[0].x += x_move;
-            knots[0].y += y_move;
-            for (int k = 1; k <= knots.size()-1; k++) {
-                move_knot(knots, k-1, k, tail_visited);
+            knots1[0].x += x_move;
+            knots1[0].y += y_move;
+            move_knot(knots1, 0, 1, tail_visited1);
+        }
+        for (int i = 0; i < move_amount; i++) {
+            knots2[0].x += x_move;
+            knots2[0].y += y_move;
+            for (int k = 1; k <= 9; k++) {
+                move_knot(knots2, k-1, k, tail_visited2);
             }
-            print_grid(knots);
         }
 
     }
     ifs.close();
 
-    vector<GridPoint> unique;
-    for (GridPoint g : tail_visited) {
-        if (find(unique.begin(), unique.end(), g) == unique.end())
-            unique.push_back(g);
+    vector<GridPoint> unique1;
+    for (GridPoint g : tail_visited1) {
+        if (find(unique1.begin(), unique1.end(), g) == unique1.end())
+            unique1.push_back(g);
     }
-
+    vector<GridPoint> unique2;
+    for (GridPoint g : tail_visited2) {
+        if (find(unique2.begin(), unique2.end(), g) == unique2.end())
+            unique2.push_back(g);
+    }
     
-    cout << "Part 1: " << unique.size() << endl;
-    //cout << "Part 2: " << endl;
+    cout << "Part 1: " << unique1.size() << endl;
+    cout << "Part 2: " << unique2.size() << endl;
 
     return 0;
 }
@@ -115,37 +125,4 @@ void move_knot(vector<GridPoint> & knots, int head_index, int tail_index, vector
         tail_visited.push_back(knots[knots.size()-1]);
 
     return;
-}
-
-void print_grid(vector<GridPoint> const& knots) {
-    for (int y = 4; y >= 0; y--) {
-        for (int x = 0; x <= 5; x++) {
-            GridPoint p = GridPoint {x,y};
-            if (p == knots[0]) {
-                cout << "H";
-            } else if (p == knots[1]) {
-                cout << "1";
-            } else if (p == knots[2]) {
-                cout << "2";
-            } else if (p == knots[3]) {
-                cout << "3";
-            } else if (p == knots[4]) {
-                cout << "4";
-            } else if (p == knots[5]) {
-                cout << "5";
-            } else if (p == knots[6]) {
-                cout << "6";
-            } else if (p == knots[7]) {
-                cout << "7";
-            } else if (p == knots[8]) {
-                cout << "8";
-            } else if (p == knots[9]) {
-                cout << "9";
-            } else {
-                cout << ".";
-            }        
-        }
-        cout << endl;
-    }
-    cout << endl;
 }
