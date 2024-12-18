435e70aa70c4e07858fd68eca9c5041ee7bf10ee
cbspace <24204459+cbspace@users.noreply.github.com>
2024-12-18 22:14:19 +1000

    Day 8 Completed

--- 2024-Python/day8.py
+++ 2024-Python/day8.py
@@ -4,35 +4,40 @@
 
 class MapGrid():
     def __init__(self, map_data):
-        self.grid = []
+        self.grid1, self.grid2 = [], []
         self.antennas = {}
-        self.antinodes = 0
+        self.antinodes1, self.antinodes2 = 0, 0
 
         for y,line in enumerate(map_data.split('\n')):
             line = line.strip()
             if len(line):
-                self.grid.append([p for p in line])
+                self.grid1.append([p for p in line])
+                self.grid2.append([p for p in line])
                 for x,p in enumerate(line):
                     if p != '.':
                         if not p in self.antennas:
                             self.antennas[p] = [[y,x]]
                         else:
                             self.antennas[p].append([y,x])
-        self.y_size = len(self.grid)
-        self.x_size = len(self.grid[0])
+        self.y_size = len(self.grid1)
+        self.x_size = len(self.grid1[0])
     
     def print_grid(self):
-        for line in self.grid:
+        for line in self.grid1:
             print(''.join(line))
 
     def location_valid(self, loc):
         return 0 <= loc[0] < self.y_size and 0 <= loc[1] < self.x_size
 
-    def update(self, loc, val):
-        if self.grid[loc[0]][loc[1]] != '#':
-            self.antinodes += 1
-            self.grid[loc[0]][loc[1]] = val
+    def update(self, loc, part2=False):
+        grid = self.grid2 if part2 else self.grid1
 
+        if grid[loc[0]][loc[1]] != '#':
+            grid[loc[0]][loc[1]] = '#'
+            if part2:
+                self.antinodes2 += 1
+            else:
+                self.antinodes1 += 1
 
 file_data = open('input/day8_input.txt').read()
 grid = MapGrid(file_data)
@@ -46,9 +51,18 @@ for antenna_group in grid.antennas.keys():
                 ant2 = antennas[idx2]
                 delta_y = ant2[0] - ant1[0]
                 delta_x = ant2[1] - ant1[1]
+
                 new_location = [ant1[0] - delta_y, ant1[1] - delta_x]
                 if grid.location_valid(new_location):
-                    grid.update(new_location, '#')
+                    grid.update(new_location)
+
+                i = 1
+                new_location = [ant1[0], ant1[1]]
+                while grid.location_valid(new_location):
+                    grid.update(new_location, True)
+                    new_location = [ant1[0] - i*delta_y, ant1[1] - i*delta_x]
+                    i = i + 1
 
 grid.print_grid()
-print(f'Part 1: {grid.antinodes}')
+print(f'Part 1: {grid.antinodes1}')
+print(f'Part 2: {grid.antinodes2}')
